2b1873ef96e2acfb293594b40da0dcda431c4a0c
martsve <martsve@gmail.com>
2021-03-03 00:06:30 +0100

    modularization

--- js-client/js/main.js
+++ js-client/js/main.js
@@ -1,79 +1,30 @@
 import mkmapi from './mkmapi.js';
-import ParseCsv from './parse-deck/ParseCsv.js';
-import matchProduct from './product-matcher.js';
-import convertToStock from './convertToStock.js';
+
+window.state = {};
+window.methods = {};
+
+window.getStored = (key, defaultObject) => {
+    try {
+        return JSON.parse(localStorage.getItem(key)) || defaultObject;
+    }
+    catch {
+        return defaultObject;
+    }
+}
 
 let main = () => {
-    let tokenStorageKey = "token";
-    let baseUrl = "https://api.cardmarket.com/ws";
-    let apiUrl = "v2.0/";
-    let idGame = 1;
-    let idLanguage = 1;
-
-    let getStored = (key, defaultObject) => {
-        try {
-            return JSON.parse(localStorage.getItem(key)) || defaultObject;
-
-        }
-        catch {
-            return defaultObject;
-        }
-    }
-
-    let token = getStored(tokenStorageKey, {
-        AppToken: '',
-        AppSecret: '',
-        AccessToken: '',
-        AccessSecret: '',
-        Url: baseUrl + '/' + apiUrl,
-    });
-
-    let active = (window.location.hash.split('#/')[1] || 'token').split('/')[0];
-
     let data = {
+        ...window.state,
         loaded: true,
-        token: token,
-        tokenTestResponse: null,
-        tokenTestResult: null,
-        active: active,
-        tabs: [
-            { key: 'token', title: 'Token setup' }, 
-            { key: 'stock', title: 'Stock' },
-            { key: 'products', title: 'Products' },
-            { key: 'import', title: 'Import' },
-        ],
-        stock: getStored('stock'),
-        stockLoading: 'ok',
-        products: getStored('products'),
-        productLoading: 'ok',
-        newArticle: '',
-        metaProductLoading: 'ok',
-        metaProducts: getStored('metaProducts'),
-        importText: '',
-        parsedItem: [],
-        savedItems: getStored('savedItems', []),
     };
-
-    for (var item of data.savedItems.filter(x => x.status == 'lookup' || x.status == 'waiting' || x.status == 'ok')) {
-        item.status = '';
-    }
-
-    const Root = { template: '<div>root</div>' }
-    const Foo = { template: '<div>foo</div>' }
-    const Bar = { template: '<div>bar</div>' }
-    const router = new VueRouter([
-        { path: '/', name: 'root', component: Root },
-        { path: '/foo', name: 'foo', component: Foo },
-        { path: '/bar', name: 'foo', component: Bar }
-    ])
 
     console.info("[MAIN] Starting vue..");
 
     var app = new Vue({
-        router,
         el: '#app',
         data,
         methods: {
+            ...window.methods,
             put: function (resource, data) {
                 return this.send('put', resource, data);
             },
@@ -94,36 +45,6 @@ let main = () => {
                     data: data
                 });
             },
-            saveToken: function () {
-                localStorage.setItem(tokenStorageKey, JSON.stringify(this.token));
-            },
-            resetToken: function () {
-                localStorage.removeItem(tokenStorageKey);
-                window.location.reload();
-            },
-            testToken: async function () {
-                var response = await this.get('account');
-                this.tokenTestResult = !response.error;
-                this.tokenTestResponse = JSON.stringify(response.error ? response.error : response.data.account, null, 2);
-                return this.tokenTestResult;
-            },
-            setActive: function (tab) {
-                this.active = tab;
-                window.location.hash = '/' + tab;
-            },
-            getStock: async function () {
-                let result = await this.get('stock');
-                console.log(JSON.stringify(result, null, 4));
-                this.stockLoading = 'loading';
-                if (result.data) {
-                    this.stock = result.data.article;
-                    this.stockLoading = 'ok';
-                    this.save();
-                }
-                else {
-                    this.stockLoading = 'error';
-                }
-            },
             getProducts: async function () {
                 let result = await this.get('productlist');
                 this.productLoading = 'loading';
@@ -140,21 +61,7 @@ let main = () => {
                 else {
                     this.productLoading = 'error';
                 }                
-            },
-            productSearch: async function(name) {
-                this.productLoading = 'loading';
-                this.products = await this.search(name);
-                console.log(JSON.stringify(this.products));
-                this.productLoading = 'ok';
-                this.save();
-            },
-            metaSearch: async function(name) {
-                this.metaProductLoading = 'loading';
-                this.metaProducts = await this.searchMeta(name);
-                console.log(JSON.stringify(this.metaProducts));
-                this.metaProductLoading = 'ok';
-                this.save();
-            },
+            },            
             search: async function (name) {
                 let parameters = {
                     search: name.split('//')[0].trim(),
@@ -184,121 +91,6 @@ let main = () => {
                 localStorage.setItem('metaProducts', JSON.stringify(this.metaProducts));
                 localStorage.setItem('savedItems', JSON.stringify(this.savedItems));
             },
-            parseData: function(data) {
-                if (!data) {
-                    return [];
-                }
-                let val = ParseCsv(data);
-                console.log('parsed', JSON.stringify(val, null, 2));
-                return val;
-            },
-            importNew: function(items) {
-                for (var item of items) {
-                    this.savedItems.push(item);   
-                }
-                this.save();
-            },
-            clearUnimported: function(items) {
-                this.savedItems = [];
-                this.save();
-            },
-            clearIds: function() {
-                for (var item of this.savedItems) {
-                    item.id = undefined;
-                }
-                this.save();
-            },
-            lookupMissingItems: function () {
-                for (var item of this.savedItems.filter(x => !x.id)) {
-                    item.status = 'waiting'
-                }
-
-                for (var i = 0; i < 5; i++) {
-                    this.lookupItem();
-                }
-            },
-            lookupItem: async function () {
-                let item = this.savedItems.filter(x => x.status === 'waiting')[0];
-                if (!item) {
-                    return;
-                }
-
-                item.status = 'lookup';
-                let meta = await this.searchMeta(item.name);
-                var match = matchProduct(item, meta);
-                item.status = match.status;
-                item.errors = match.errors;
-                item.warnings = match.warnings;
-                item.id = match.id;
-                if (match.setName) {
-                    item.oldSetName = item.setName;
-                    item.setName = match.setName;
-                    item.altSet = match.altSet;
-                }
-                if (match.name) {
-                    item.oldName = item.name;
-                    item.name = match.name;
-                    item.altName = match.altName;
-                }
-
-                this.save();
-                this.lookupItem();
-            },
-            resetStatus: function (item) {
-                item.status = '';
-                item.errors = [];
-                item.warnings = [];
-                this.save();
-            },
-            importToCardmarket: async function() {
-                var items = this.savedItems.filter(x => x.id && x.errors.length == 0 && x.warnings.length == 0);
-                if (!items.length) { return; }
-
-                let ii = 0;
-                let itemMap = {};
-                for (var item of items) {
-                    item.importId = (ii++).toString();
-                    itemMap[item.importId] = item;
-                    items.status = 'lookup';
-                }
-
-                var stock = convertToStock(items);
-                console.log('stock', JSON.stringify(stock, null, 2));
-                var result = await this.post('stock', stock);
-                console.log('result', JSON.stringify(result, null, 2));
-                var inserted = result?.data?.inserted || [];
-                var i = 0;
-                for (var item of inserted) {
-                    if (item.success) {
-                        let iid = item.idArticle.comments.match(/\[(\d+)\]/)[1];
-                        itemMap[iid].inserted = true;
-                    }
-                    else {
-                        let iid = item.tried.comments.match(/\[(\d+)\]/)[1];
-                        itemMap[iid].status = 'error';
-                        itemMap[iid].errors.push({ key: 'stock', value: item.error });
-                    }
-                    i++;
-                }
-
-                this.savedItems = this.savedItems.filter(x => !x.inserted);
-                this.save();
-            },
-            lookupProduct: async function (id) {
-                let data = (await this.get('products/' + id))?.data?.product;
-                console.log(JSON.stringify(data));
-            },
-            getPrices: async function() {
-                //var result = await this.get('games/1/expansions');
-                //var list = result.data.expansion.map(x => ({ id: x.idExpansion, name: x.enName }));
-                let expansions = new Map();
-                for (let item of this.stock) {
-                    expansions.set(item.product.expIcon, 'ok');
-                }
-                let expIds = Array.from(expansions.keys());
-                let res = await this.get('expansions/' + expIds[0] + '/singles');
-                console.log(res);
-            }
         }
     });
 
--- js-client/js/preload.js
+++ js-client/js/preload.js
@@ -1,9 +1,50 @@
+window.loadedScripts = 0;
+window.totalScripts = 0;
+window.finishedLoading = false;
+window.checkFinished = (inc) => {
+    window.loadedScripts = window.loadedScripts + inc;
+    if (window.loadedScripts === window.totalScripts && window.finishedLoading) {
+        console.log('[PRELOAD] Scripts finished loading.');
+        document.dispatchEvent(new Event('preload-finished'));
+    }
+    else {
+        console.log('[PRELOAD] Waiting for all scripts to load (' + window.loadedScripts + '/' + window.totalScripts + ')');
+    }
+};
+
+let duplicateTag = (domElem, type) => {
+    var script = document.createElement(type);
+    for (var attr of domElem.attributes) {
+        script.setAttribute(attr.name, attr.value);
+    }
+
+    script.innerHTML = domElem.innerHTML;
+    if (type == "SCRIPT") {
+        window.totalScripts++;
+        script.innerHTML += "\window.checkFinished(1);";
+    }
+
+    return script;
+}
+
 let preload = async (elem) => {
     let url = elem.getAttribute('preload');
+    let cd = url.split('/').slice(0,-1).join('/')+'/';
     let content = await fetch(url);
     let html = await content.text();
+
+    html = html.replace(/\@\{CD\}/ig, cd);
+
+    let div = document.createElement("div");
+    div.innerHTML = html;
+    let domObjects = div.querySelectorAll('script, link, style');
+    for (var obj of domObjects) {
+        obj.parentNode.removeChild(obj);
+        document.body.appendChild(duplicateTag(obj, obj.tagName));
+    }
+
     elem.removeAttribute('preload');
-    elem.innerHTML = html;
+    elem.innerHTML = div.innerHTML;
 };
 
 let preloadDefaults = async () => {
@@ -19,7 +60,8 @@ let preloadDefaults = async () => {
     }
     else {
         console.log('[PRELOAD] Preload finished.');
-        document.dispatchEvent(new Event('preload-finished'));
+        window.finishedLoading = true;
+        window.checkFinished(0);
     }
 }
 
